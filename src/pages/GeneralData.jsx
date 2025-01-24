6c152366418a5aa27f029741c26b57f721d652cd
eusouopeu <pedro.caio@ufba.br>
2025-01-24 18:48:34 -0300

    .

--- src/pages/GeneralData.jsx
+++ src/pages/GeneralData.jsx
@@ -64,75 +64,75 @@ export default function GeneralData() {
 							className='w-full grid lg:grid-cols-2'
 						>
 							<Grid className='grid col-span-1 gap-4'>
-						<Typography
-							variant='h2'
+								<Typography
+									variant='h2'
 									className='w-full text-center text-2xl font-[700] text-orange-600'
-						>
-							Sobre o bairro
-						</Typography>
+								>
+									Sobre o bairro
+								</Typography>
 
-						<Grid
-							container
-							spacing={2}
+								<Grid
+									container
+									spacing={2}
 									className='grid lg:grid-cols-2'
-						>
-							<DashCard
-								icon={<UsersIcon height={72} />}
-								title='População'
-								main='2.584'
-								detail='habitantes'
-							/>
+								>
+									<DashCard
+										icon={<UsersIcon height={72} />}
+										title='População'
+										main='2.584'
+										detail='habitantes'
+									/>
 
-							<DashCard
-								icon={<MapIcon height={72} />}
-								title='Área'
-								main='2.584'
-								detail='km2'
-							/>
-							<DashCard
+									<DashCard
+										icon={<MapIcon height={72} />}
+										title='Área'
+										main='2.584'
+										detail='km2'
+									/>
+									<DashCard
 										icon={
 											<BuildingOffice2Icon height={72} />
 										}
-								title='Densidade Populacional'
-								main='2.584'
-								detail='habitantes / km2'
-							/>
+										title='Densidade Populacional'
+										main='2.584'
+										detail='habitantes / km2'
+									/>
 
-							<DashCard
-								title='Prefeitura-bairro'
-								other='Barra-Pituba'
-							/>
-						</Grid>
+									<DashCard
+										title='Prefeitura-bairro'
+										other='Barra-Pituba'
+									/>
+								</Grid>
 							</Grid>
 							<Grid className='grid col-span-1 grid-cols-1 w-full md:h-full gap-4'>
-						<Typography
-							variant='h2'
+								<Typography
+									variant='h2'
 									className='w-full text-center text-2xl font-[700] text-orange-600'
-						>
-							Panorama demográfico
-						</Typography>
+								>
+									Panorama demográfico
+								</Typography>
 
-						<Grid
-							container
-							spacing={2}
+								<Grid
+									container
+									spacing={2}
 									className='col-span-1 flex flex-col md:h-full lg:justify-between '
-						>
-							<DashCard
-								title='%dd Genero'
-								main={<MuiPieChart data={chartData} />}
-							/>
+								>
+									<DashCard
+										title='%dd Genero'
+										main={<MuiPieChart data={chartData} />}
+									/>
 
-							<DashCard
-								icon={<MapIcon height={72} />}
-								title='% Etnia'
-								//ddd
-							/>
-							<DashCard
+									<DashCard
+										icon={<MapIcon height={72} />}
+										title='% Etnia'
+										//ddd
+									/>
+									<DashCard
 										icon={
 											<BuildingOffice2Icon height={72} />
 										}
-								title='Faixa Etária'
-							/>
+										title='Faixa Etária'
+									/>
 									{/* <DashCard
 										icon={
 											<BuildingOffice2Icon height={72} />

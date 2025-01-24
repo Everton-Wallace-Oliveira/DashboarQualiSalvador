8d8ac6bab4e178e948dc981919d92b261438921b
Leo <alessandrooliveira645.ao@gmail.com>
2025-01-24 16:59:45 -0300

    git

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -6,30 +6,7 @@ const Sidebar = () => {
       <div className="mb-5">
         <div className="w-20 h-20 bg-white rounded-full"></div>
       </div>
-      <nav className="w-full">
-        <ul className="list-none p-0 m-0">
-          <li className="py-2 px-5 hover:bg-yellow-400 hover:text-black cursor-pointer">Início</li>
-          <li className="py-2 px-5 hover:bg-yellow-400 hover:text-black cursor-pointer">
-            Info. Demográficas
-          </li>
-          <li className="py-2 px-5 hover:bg-yellow-400 hover:text-black cursor-pointer relative group">
-            Dados por Gênero
-            <ul className="hidden group-hover:block bg-orange-600 absolute left-full top-0 list-none pl-5">
-              <li className="py-1 px-3 hover:bg-orange-500">Saúde</li>
-              <li className="py-1 px-3 hover:bg-orange-500">Educação</li>
-              <li className="py-1 px-3 hover:bg-orange-500">Segurança</li>
-            </ul>
-          </li>
-          <li className="py-2 px-5 hover:bg-yellow-400 hover:text-black cursor-pointer relative group">
-            Dados por Etnia
-            <ul className="hidden group-hover:block bg-orange-600 absolute left-full top-0 list-none pl-5">
-              <li className="py-1 px-3 hover:bg-orange-500">Saúde</li>
-              <li className="py-1 px-3 hover:bg-orange-500">Educação</li>
-              <li className="py-1 px-3 hover:bg-orange-500">Segurança</li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+      
     </div>
   );
 };
